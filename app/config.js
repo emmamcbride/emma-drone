b68f3cc380b4193227b42696dd250de70022cc0a
Emma McBride <emma.mcbride@digital.homeoffice.gov.uk>
2019-09-20 10:43:56 +0100

    Added register route

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register your drone',
+  serviceName: 'Registering a drone',
 
   // Default port that prototype runs on
   port: '3000',
